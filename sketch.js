7230411d7f8ce55787fdc47b4bfc79f496924284
Akan1994 <ugc1994.aka@gmail.com>
2020-08-12 09:56:43 +0530

    no change

--- sketch.js
+++ sketch.js
@@ -14,6 +14,8 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
+    platform=new Ground(200,300,40,40);
+    
 
 
     ground = new Ground(600,height,1200,20);
